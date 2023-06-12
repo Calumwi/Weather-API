e668da9f1a1fb5db3b4d677204bfaf4fab4cf1a9
Calum Wilmot <calumdefences@gmail.com>
2023-06-12 16:50:43 +0100

    API Complete

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express'); // import express
 const app = express(); // initialize express
+const cors = require('cors'); // import cors package
+const fakeWeatherData = require('./data.js'); //import fake data
+
+app.use(cors());
+
+// GET route 
+app.get('/weather', function(req, res) {
+  let cityName = req.query.city.toLowerCase()
+
+  for (let i = 0; i < fakeWeatherData.length; i++)
+
+  if (!cityName) {
+    return res.send({"status": "error", "message": "Please enter a city name"})
+  }
+  else if (cityName == fakeWeatherData[i].city.toLowerCase()) {
+    return res.send(fakeWeatherData[i])
+  }
+
+res.send({"status": "error", "message": "This city isn't in our database"})
+});
+
 
 // Listen on port 3000
 app.listen(3000, function() {
